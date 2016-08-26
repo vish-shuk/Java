819008af51f97c83fa5ccd7260dc4b3b66272fac
vish-shuk <vishakha.shukla1996@gmail.com>
2016-08-26 19:49:33 +0530

    Add files via upload

---
+++ Prob.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+public class Prob {
+    public static void main(String[] args)  {
+    
+        Random number = new Random();
+        int x;
+        double s, probability;
+        s = 0;
+        
+        for (int i = 0; i < 1000; i++) {
+            x = number.nextInt(100) + 1;
+            if (x % 2 == 0) {
+                s = s + 1;
+            }
+        }
+        probability = s/100;
+        System.out.println(probability);
+    }
+}
+            
